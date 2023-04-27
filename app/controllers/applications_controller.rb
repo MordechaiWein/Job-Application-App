7e44ef458cc7e82ccf01d178ec8646a6cca0e47d
Mordechai <mordwein77@gmail.com>
2023-04-27 17:18:09 -0400

    this will dry up the applications controller

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,34 +1,25 @@
 class ApplicationsController < ApplicationController
+    rescue_from ActiveRecord::RecordInvalid, with: :render_application_errors
     
-
     def index
         user = User.find_by(id: session[:user_id])
         render json: user.applications
     end
-  
-
+    
     def create
         user = User.find_by(id: session[:user_id])
-       
-            application = user.applications.create(strong_params)
-            if application.valid?
-               render json: application, status: :created
-            else
-                render json: {errors: application.errors.full_messages}, status: :unprocessable_entity
-            end
-     
-         
-      
+        application = user.applications.create(strong_params)
+        if application.valid?
+            render json: application, status: :created
+        else
+            render json: {errors: application.errors.full_messages}, status: :unprocessable_entity
+        end
     end
 
     def update
         application = Application.find_by(id: params[:id])
-        application.update(strong_params)
-        if application.valid?
-            render json: application
-        else
-            render json: {errors: application.errors.full_messages}, status: :unprocessable_entity
-        end
+        application.update!(strong_params)
+        render json: application
     end
 
     def destroy
@@ -41,6 +32,10 @@ class ApplicationsController < ApplicationController
 
     def strong_params
         params.permit(:first_name, :last_name, :age, :work_experience, :job_id, :phone_number, :email_address, :application_name)
+    end
+
+    def render_application_errors(instance)
+        render json: {errors: instance.record.errors.full_messages }, status: :unprocessable_entity
     end
 
 end

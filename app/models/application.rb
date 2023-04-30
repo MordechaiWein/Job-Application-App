eb7324bc05c7b00e5997e6cad47fa1b9a20f80b5
Mordechai <mordwein77@gmail.com>
2023-04-30 13:51:26 -0400

    this will add a validation for correct phone number syntax

--- app/models/application.rb
+++ app/models/application.rb
@@ -4,6 +4,7 @@ class Application < ApplicationRecord
 
     validates :first_name, :last_name, :age, :work_experience, :phone_number, :email_address, presence: true
     validates :work_experience, length: {maximum: 250}
+    validates :phone_number, format: { with: /\A(\([0-9]{3}\)|[0-9]{3}-)[0-9]{3}-[0-9]{4}\z/ }
 end
 
 

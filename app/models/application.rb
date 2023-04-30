6f83b0430a8a5df3132746a8f3da4f40c07de972
Mordechai <mordwein77@gmail.com>
2023-04-30 13:53:43 -0400

    this will add a validation for correct email syntax

--- app/models/application.rb
+++ app/models/application.rb
@@ -5,6 +5,7 @@ class Application < ApplicationRecord
     validates :first_name, :last_name, :age, :work_experience, :phone_number, :email_address, presence: true
     validates :work_experience, length: {maximum: 250}
     validates :phone_number, format: { with: /\A(\([0-9]{3}\)|[0-9]{3}-)[0-9]{3}-[0-9]{4}\z/ }
+    validates :email_address, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i }
 end
 
 

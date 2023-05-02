387087b306c5e926108283e79a1dd92d2252bd96
Mordechai <mordwein77@gmail.com>
2023-05-02 01:48:52 -0400

    this will reseed my database

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,5 +4,6 @@ Job.create(name: "Dog Watcher", job_description: "need someone to watch my dog d
 Job.create(name: "Personal Trainer", job_description: "looking for someone who can help me exercise", pay: "75 dollars a session")
 Job.create(name: "Gardener", job_description: "need someone to make me a garden", pay: "500 dollars for the entire job")
 Job.create(name: "Tutor", job_description: "need someone to teach me python", pay: "20 dollars an hour")
+User.create(username: "Mordechai", password: "bella888", image_url: "https://i.natgeofe.com/k/02444b59-a50d-48e6-939b-4db10f895e66/5-reasons-eagle_4x3.jpg", bio: "Creator of New Career Network", admin: true)
 
 puts "✅ done seeding"
